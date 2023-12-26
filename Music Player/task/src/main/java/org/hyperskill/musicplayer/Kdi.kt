a310e4ef1f31522e246da2cdf70b45822d422d97
Zhanarys94 <janaris1994@gmail.com>
2023-12-26 17:10:57 +0600

    Proceeded to the stage 3: -Added tests for stage 3

--- Music Player/task/src/main/java/org/hyperskill/musicplayer/Kdi.kt
+++ Music Player/task/src/main/java/org/hyperskill/musicplayer/Kdi.kt
@@ -1,19 +1,9 @@
 package org.hyperskill.musicplayer
-/*
 
-import android.app.Application
-import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.startKoin
+/*import org.hyperskill.musicplayer.model.PlaylistsRepositoryImpl
 import org.koin.dsl.module
 
-class Kdi : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        startKoin {
-            androidContext(this@MusicAll)
-            modules(listOf(
-                repositoryModules
-            ))
-        }
-    }
+val repositoryModules = module {
+    single { PlaylistsRepositoryImpl() }
 }*/
+
--- Music Player/task/src/main/java/org/hyperskill/musicplayer/MainActivity.kt
+++ Music Player/task/src/main/java/org/hyperskill/musicplayer/MainActivity.kt
@@ -51,7 +51,7 @@ class MainActivity : AppCompatActivity(),
         }
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
+
 
         val recyclerView = binding.mainSongList
         recyclerView.layoutManager = LinearLayoutManager(this)
